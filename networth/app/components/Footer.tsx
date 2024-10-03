b1a261d93068d7f185455fbd640707526d4138fe
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-10-04 01:15:12 +0530

    navbar-changes

--- networth/app/components/Footer.tsx
+++ networth/app/components/Footer.tsx
@@ -8,37 +8,37 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-4">About Us</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-blue-400">Our Story</a></li>
-            <li><a href="#" className="hover:text-blue-400">Team</a></li>
-            <li><a href="#" className="hover:text-blue-400">Careers</a></li>
+            <li><Link href="#" className="hover:text-blue-400">Our Story</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">Team</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">Careers</Link></li>
           </ul>
         </div>
         <div>
           <h3 className="text-lg font-semibold mb-4">Resources</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-blue-400">Blog</a></li>
-            <li><a href="#" className="hover:text-blue-400">FAQ</a></li>
-            <li><a href="#" className="hover:text-blue-400">Support</a></li>
+            <li><Link href="#" className="hover:text-blue-400">Blog</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">FAQ</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">Support</Link></li>
           </ul>
         </div>
         <div>
           <h3 className="text-lg font-semibold mb-4">Legal</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-blue-400">Privacy Policy</a></li>
-            <li><a href="#" className="hover:text-blue-400">Terms of Service</a></li>
+            <li><Link href="#" className="hover:text-blue-400">Privacy Policy</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">Terms of Service</Link></li>
           </ul>
         </div>
         <div>
           <h3 className="text-lg font-semibold mb-4">Connect</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-blue-400">Twitter</a></li>
-            <li><a href="#" className="hover:text-blue-400">LinkedIn</a></li>
-            <li><a href="#" className="hover:text-blue-400">Facebook</a></li>
+            <li><Link href="#" className="hover:text-blue-400">Twitter</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">LinkedIn</Link></li>
+            <li><Link href="#" className="hover:text-blue-400">Facebook</Link></li>
           </ul>
         </div>
       </div>
       <div className="mt-8 text-center">
-        <p>&copy; 2024 ReferAl. All rights reserved.</p>
+        <p> Referrly &copy;{new Date().getFullYear()}&nbsp; All rights reserved.</p>
       </div>
     </div>
   </footer>
--- networth/app/components/Navbar.tsx
+++ networth/app/components/Navbar.tsx
@@ -5,11 +5,12 @@ import { onAuthStateChanged, signOut, User } from "firebase/auth";
 import { FiUser, FiMenu, FiX } from "react-icons/fi";
 import Link from "next/link";
 import { doc, getDoc } from "firebase/firestore";
-import logo from '../../public/assets/Logo.jpg';
+import logo from "../../public/assets/Logo.jpg";
 
 const Navbar = () => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [role, setRole] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false); // For hamburger menu toggle
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -22,10 +23,12 @@ const Navbar = () => {
         const userDoc = await getDoc(doc(firestore, "users", user.uid));
         if (userDoc.exists()) {
           setRole(userDoc.data().role);
+          setName(userDoc.data().name);
         }
       } else {
         setCurrentUser(null);
         setRole(null);
+        setName(null);
       }
     });
 
@@ -59,13 +62,24 @@ const Navbar = () => {
     };
   }, []);
 
+  const displayName = currentUser?.displayName
+    ? `Welcome ${currentUser.displayName}`
+    : name
+    ? `Welcome ${name}`
+    : "Account";
+
   return (
     <header className="bg-white shadow-md relative">
-      <div className="container mx-auto px-4 py-6 flex items-center justify-between">
-        {/* Logo on the left */}
+      <div className="mx-auto px-4 py-6 flex items-center justify-between">
         <div className="flex items-center">
-          <Image src={logo} alt="Logo" width={50} height={50} className="object-contain m-2" />
-          <h1 className="text-2xl font-bold text-teal-600">Referrly</h1>
+          <Image
+            src={logo}
+            alt="Logo"
+            width={50}
+            height={50}
+            className="object-contain m-2"
+          />
+          <h1 className="text-3xl font-bold text-teal-600">Referrly</h1>
         </div>
 
         {/* Hamburger icon for small screens */}
@@ -80,16 +94,49 @@ const Navbar = () => {
         </div>
 
         {/* Navigation for large screens */}
-        <nav className={`hidden lg:flex-grow lg:flex lg:justify-center`}>
+        <nav className={`hidden lg:flex-grow lg:flex lg:justify-end`}>
           <ul className="flex space-x-6">
             <li>
-              <Link href="#" className="text-gray-600 hover:text-teal-600">How it Works</Link>
+              <Link href="#" className=" text-gray-600 hover:text-teal-600" style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                Who are we?
+              </Link>
             </li>
             <li>
-              <Link href="#" className="text-gray-600 hover:text-teal-600">For Referrers</Link>
+              <Link href="#" className="text-gray-600 hover:text-teal-600" style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                How it Works
+              </Link>
             </li>
             <li>
-              <Link href="#" className="text-gray-600 hover:text-teal-600">For Referees</Link>
+              <Link href="#" className="text-gray-600 hover:text-teal-600" style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                Testimonials
+              </Link>
             </li>
           </ul>
         </nav>
@@ -99,68 +146,124 @@ const Navbar = () => {
           <nav className="lg:hidden absolute top-full left-0 w-full bg-white shadow-md">
             <ul className="flex flex-col items-center space-y-4 py-4">
               <li>
-                <Link href="#" className="text-gray-600 hover:text-teal-600">How it Works</Link>
-              </li>
-              <li>
-                <Link href="#" className="text-gray-600 hover:text-teal-600">For Referrers</Link>
-              </li>
-              <li>
-                <Link href="#" className="text-gray-600 hover:text-teal-600">For Referees</Link>
-              </li>
-              {/* <li> */}
-            {currentUser ? (
-            <div className="relative">
-              <div
-                ref={accountRef}
-                className="flex items-center cursor-pointer p-2 rounded-md hover:bg-gray-100"
-                onMouseEnter={() => setIsDropdownVisible(true)}
-              >
-                <FiUser className="text-teal-600 mr-2" />
-                <span className="text-teal-600">
-                  {currentUser.displayName || "Account"}
-                </span>
-              </div>
-              {isDropdownVisible && (
-                <div
-                  ref={dropdownRef}
-                  className="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg z-10"
-                  onMouseLeave={() => setIsDropdownVisible(false)}
-                >
-                  <ul className="py-2">
-                    <li>
-                      <Link href={role === "referrer" ? "/referrer_dashboard" : "/profile"} className="block px-4 py-2 text-gray-700 hover:bg-teal-50">
-                        {role === "referrer" ? "View Dashboard" : "Profile Settings"}
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/applications" className="block px-4 py-2 text-gray-700 hover:bg-teal-50">My Applications</Link>
-                    </li>
-                    <li>
-                      <button onClick={handleLogout} className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-teal-50">Logout</button>
-                    </li>
-                  </ul>
-                </div>
-              )}
-            </div>
-          ) : (
-            <ul className="flex flex-col items-center space-y-4 py-4">
-              <li>
-                <Link href="/login" className="text-gray-600 hover:text-teal-600">Sign In</Link>
-              </li>
-              <li>
-                <Link href="/signup">
-                  <div className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700">Sign Up</div>
+                <Link href="#" className="text-gray-600 hover:text-teal-600" style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                  Who are we?
                 </Link>
               </li>
+              <li>
+                <Link href="#" className="text-gray-600 hover:text-teal-600"style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                  How it Works
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="text-gray-600 hover:text-teal-600"style={{
+                  borderBottom: "2px solid transparent",
+                  transition: "border-color 0.3s",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "teal")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.borderBottomColor = "transparent")
+                }>
+                  Testimonials
+                </Link>
+              </li>
+              {/* <li> */}
+              {currentUser ? (
+                <div className="relative">
+                  <div
+                    ref={accountRef}
+                    className="flex items-center cursor-pointer p-2 rounded-md hover:bg-gray-100"
+                    onMouseEnter={() => setIsDropdownVisible(true)}
+                  >
+                    <FiUser className="text-teal-600 mr-2" />
+                    <span className="text-teal-600">
+                      {displayName}
+                    </span>
+                  </div>
+                  {isDropdownVisible && (
+                    <div
+                      ref={dropdownRef}
+                      className="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg z-10"
+                      onMouseLeave={() => setIsDropdownVisible(false)}
+                    >
+                      <ul className="py-2">
+                        <li>
+                          <Link
+                            href={
+                              role === "referrer"
+                                ? "/referrer_dashboard"
+                                : "/profile"
+                            }
+                            className="block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                          >
+                            {role === "referrer"
+                              ? "View Dashboard"
+                              : "Profile Settings"}
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/applications"
+                            className="block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                          >
+                            My Applications
+                          </Link>
+                        </li>
+                        <li>
+                          <button
+                            onClick={handleLogout}
+                            className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                          >
+                            Logout
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <ul className="flex flex-col items-center space-y-4 py-4">
+                  <li>
+                    <Link
+                      href="/login"
+                      className="text-gray-600 hover:text-teal-600"
+                    >
+                      Sign In
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/signup">
+                      <div className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700">
+                        Sign Up
+                      </div>
+                    </Link>
+                  </li>
+                </ul>
+              )}
+              {/* </li> */}
             </ul>
-          )}
-          {/* </li> */}
-          </ul>
           </nav>
-          
-        )
-        
-        }
+        )}
 
         {/* User Account/Sign In - Large Screens */}
         <div className="hidden lg:flex lg:items-center">
@@ -173,7 +276,7 @@ const Navbar = () => {
               >
                 <FiUser className="text-teal-600 mr-2" />
                 <span className="text-teal-600">
-                  {currentUser.displayName || "Account"}
+                  {displayName}
                 </span>
               </div>
               {isDropdownVisible && (
@@ -184,15 +287,34 @@ const Navbar = () => {
                 >
                   <ul className="py-2">
                     <li>
-                      <Link href={role === "referrer" ? "/referrer_dashboard" : "/profile"} className="block px-4 py-2 text-gray-700 hover:bg-teal-50">
-                        {role === "referrer" ? "View Dashboard" : "Profile Settings"}
+                      <Link
+                        href={
+                          role === "referrer"
+                            ? "/referrer_dashboard"
+                            : "/profile"
+                        }
+                        className="block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                      >
+                        {role === "referrer"
+                          ? "View Dashboard"
+                          : "Profile Settings"}
                       </Link>
                     </li>
                     <li>
-                      <Link href="/applications" className="block px-4 py-2 text-gray-700 hover:bg-teal-50">My Applications</Link>
+                      <Link
+                        href="/applications"
+                        className="block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                      >
+                        My Applications
+                      </Link>
                     </li>
                     <li>
-                      <button onClick={handleLogout} className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-teal-50">Logout</button>
+                      <button
+                        onClick={handleLogout}
+                        className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-teal-50"
+                      >
+                        Logout
+                      </button>
                     </li>
                   </ul>
                 </div>
@@ -201,11 +323,18 @@ const Navbar = () => {
           ) : (
             <>
               <div className="ml-6">
-                <Link href="/login"className="text-gray-600 hover:text-teal-600">Sign In</Link>
+                <Link
+                  href="/login"
+                  className="text-gray-600 hover:text-teal-600"
+                >
+                  Sign In
+                </Link>
               </div>
               <div className="ml-4">
                 <Link href="/signup">
-                  <div className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700">Sign Up</div>
+                  <div className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700">
+                    Sign Up
+                  </div>
                 </Link>
               </div>
             </>
